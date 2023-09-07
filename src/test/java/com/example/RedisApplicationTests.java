23fe2f39debffd0c8c4f4600e628168d12bf1f3e
pangxiaobo <pangxiaobo@vchangyi.com>
2023-09-07 16:25:49 +0800

    update

--- src/test/java/com/example/RedisApplicationTests.java
+++ src/test/java/com/example/RedisApplicationTests.java
@@ -17,27 +17,27 @@ class RedisApplicationTests {
 
     @Test
     public void testRedis() {
-        // 添加
-        redisTemplate.opsForValue().set("name","张三");
-        // 查询
-        System.out.println(redisTemplate.opsForValue().get("name"));
-        // 删除
-        redisTemplate.delete("name");
-        // 更新
-        redisTemplate.opsForValue().set("name","张三哈哈哈");
-        // 查询
-        System.out.println(redisTemplate.opsForValue().get("name"));
-
-        // 添加
-        stringRedisTemplate.opsForValue().set("name","张三");
-        // 查询
-        System.out.println(stringRedisTemplate.opsForValue().get("name"));
-        // 删除
-        stringRedisTemplate.delete("name");
-        // 更新
-        stringRedisTemplate.opsForValue().set("name","张三的呵呵呵呵");
-        // 查询
-        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+//        // 添加
+//        redisTemplate.opsForValue().set("name","张三");
+//        // 查询
+//        System.out.println(redisTemplate.opsForValue().get("name"));
+//        // 删除
+//        redisTemplate.delete("name");
+//        // 更新
+//        redisTemplate.opsForValue().set("name","张三哈哈哈");
+//        // 查询
+//        System.out.println(redisTemplate.opsForValue().get("name"));
+//
+//        // 添加
+//        stringRedisTemplate.opsForValue().set("name","张三");
+//        // 查询
+//        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+//        // 删除
+//        stringRedisTemplate.delete("name");
+//        // 更新
+//        stringRedisTemplate.opsForValue().set("name","张三的呵呵呵呵");
+//        // 查询
+//        System.out.println(stringRedisTemplate.opsForValue().get("name"));
 
     }
 
