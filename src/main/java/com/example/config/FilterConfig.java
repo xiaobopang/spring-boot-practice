72c3adbdb115fa3353c7a712b612af262672c697
pangxiaobo <pangxiaobo@vchangyi.com>
2023-09-12 16:08:22 +0800

    过滤器

---
+++ src/main/java/com/example/config/FilterConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+
+import com.example.filter.RepeatableFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Filter配置
+ *
+ */
+@Configuration
+public class FilterConfig {
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new RepeatableFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("repeatableFilter");
+        registration.setOrder(FilterRegistrationBean.LOWEST_PRECEDENCE);
+        return registration;
+    }
+
+}
