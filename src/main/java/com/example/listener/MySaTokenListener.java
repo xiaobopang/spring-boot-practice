215ad71fd2dd225ad82fa74074508e107e53665c
pangxiaobo <pangxiaobo@vchangyi.com>
2023-09-12 15:20:43 +0800

    some update

--- src/main/java/com/example/listener/MySaTokenListener.java
+++ src/main/java/com/example/listener/MySaTokenListener.java
@@ -2,9 +2,13 @@ package com.example.listener;
 
 import cn.dev33.satoken.listener.SaTokenListenerForSimple;
 import cn.dev33.satoken.stp.SaLoginModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
+@Slf4j
 public class MySaTokenListener extends SaTokenListenerForSimple {
     /*
      * SaTokenListenerForSimple 对所有事件提供了空实现，通过继承此类，你只需重写一部分方法即可实现一个可用的侦听器。
@@ -15,13 +19,77 @@ public class MySaTokenListener extends SaTokenListenerForSimple {
      */
     @Override
     public void doLogin(String loginType, Object loginId, String tokenValue, SaLoginModel loginModel) {
-        System.out.println("---------- 自定义侦听器实现 doLogin");
+        log.info("user doLogin, userId:{}, token:{}", loginId, tokenValue);
     }
 
     @Override
     public void doLogout(String loginType, Object loginId, String tokenValue) {
-        System.out.println("---------- 自定义侦听器实现 doLogout");
+        log.info("user doLogout, userId:{}, token:{}", loginId, tokenValue);
     }
 
+    /**
+     * 每次被踢下线时触发
+     */
+    @Override
+    public void doKickout(String loginType, Object loginId, String tokenValue) {
+        log.info("user Kick-out, userId:{}, token:{}", loginId, tokenValue);
+    }
+
+    /**
+     * 每次被顶下线时触发
+     */
+    @Override
+    public void doReplaced(String loginType, Object loginId, String tokenValue) {
+        log.info("user doReplaced, userId:{}, token:{}", loginId, tokenValue);
+    }
+
+    /**
+     * 每次被封禁时触发
+     */
+    @Override
+    public void doDisable(String loginType, Object loginId, String service, int level, long disableTime) {
+    }
+
+    /**
+     * 每次被解封时触发
+     */
+    @Override
+    public void doUntieDisable(String loginType, Object loginId, String service) {
+    }
+
+    /**
+     * 每次打开二级认证时触发
+     */
+    @Override
+    public void doOpenSafe(String loginType, String tokenValue, String service, long safeTime) {
+    }
+
+    /**
+     * 每次创建Session时触发
+     */
+    @Override
+    public void doCloseSafe(String loginType, String tokenValue, String service) {
+    }
+
+    /**
+     * 每次创建Session时触发
+     */
+    @Override
+    public void doCreateSession(String id) {
+    }
+
+    /**
+     * 每次注销Session时触发
+     */
+    @Override
+    public void doLogoutSession(String id) {
+    }
+
+    /**
+     * 每次Token续期时触发
+     */
+    @Override
+    public void doRenewTimeout(String tokenValue, Object loginId, long timeout) {
+    }
 }
 
