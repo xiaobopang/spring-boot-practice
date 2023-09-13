26b2ed3abc26dc9977bc3b81fa07873cf29a4566
pangxiaobo <pangxiaobo@vchangyi.com>
2023-09-13 14:23:31 +0800

    统一异常处理

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import cn.hutool.http.HttpStatus;
 import com.example.domain.ResponseEntity;
 import com.example.utils.StreamUtils;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.mybatis.spring.MyBatisSystemException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.util.NestedServletException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
@@ -39,9 +37,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(org.springframework.http.HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Void> handleNotFound(NoHandlerFoundException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',不支持'{}'请求", requestURI, e.getHttpMethod());
+        log.error("404异常'{}',不支持'{}'请求", request.getRequestURI(), e.getHttpMethod());
         return ResponseEntity.fail(HttpStatus.HTTP_NOT_FOUND, "访问方法不存在");
+    }
+
+    /**
+     * 捕捉空指针异常
+     */
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity<Void> exceptionHandler(NullPointerException e, HttpServletRequest request) {
+        log.error("空指针异常：{}, {}", request.getRequestURI(), e.getMessage());
+        return ResponseEntity.fail("空指针异常：" + e.getMessage());
     }
 
     /**
@@ -50,8 +56,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<Void> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e,
                                                                     HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',不支持'{}'请求", requestURI, e.getMethod());
+        log.error("请求方式不支持'{}',不支持'{}'请求", request.getRequestURI(), e.getMethod());
         return ResponseEntity.fail("请求方式不支持");
     }
 
@@ -60,23 +65,22 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(DuplicateKeyException.class)
     public ResponseEntity<Void> handleDuplicateKeyException(DuplicateKeyException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',数据库中已存在记录'{}'", requestURI, e.getMessage());
+        log.error("请求地址'{}',数据库中已存在记录'{}'", request.getRequestURI(), e.getMessage());
         return ResponseEntity.fail("数据库中已存在该记录，请联系管理员确认");
     }
 
     /**
-     * Mybatis系统异常 通用处理
+     * Mybatis系统异常
      */
     @ExceptionHandler(MyBatisSystemException.class)
-    public ResponseEntity<Void> handleCannotFindDataSourceException(MyBatisSystemException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
+    public ResponseEntity<Void> handleCannotFindDataSourceException(MyBatisSystemException e,
+                                                                    HttpServletRequest request) {
         String message = e.getMessage();
         if (message != null && message.contains("CannotFindDataSourceException")) {
-            log.error("请求地址'{}', 未找到数据源", requestURI);
+            log.error("Mybatis系统异常，请求地址'{}', 未找到数据源 {}", request.getRequestURI(), message);
             return ResponseEntity.fail("未找到数据源，请联系管理员确认");
         }
-        log.error("请求地址'{}', Mybatis系统异常", requestURI, e);
+        log.error("Mybatis系统异常，请求地址'{}', Mybatis系统异常:{}", request.getRequestURI(), message);
         return ResponseEntity.fail(message);
     }
 
@@ -86,8 +90,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MissingPathVariableException.class)
     public ResponseEntity<Void> handleMissingPathVariableException(MissingPathVariableException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求路径中缺少必需的路径变量'{}',发生系统异常.", requestURI, e);
+        log.error("请求路径中缺少必需的路径变量'{}',发生系统异常.", request.getRequestURI(), e);
         return ResponseEntity.fail(String.format("请求路径中缺少必需的路径变量[%s]", e.getVariableName()));
     }
 
@@ -95,18 +98,19 @@ public class GlobalExceptionHandler {
      * 请求参数类型不匹配
      */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Void> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求参数类型不匹配'{}',发生系统异常.", requestURI, e);
+    public ResponseEntity<Void> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e,
+                                                                          HttpServletRequest request) {
+        log.error("请求参数类型不匹配'{}',发生系统异常.", request.getRequestURI(), e);
         return ResponseEntity.fail(String.format("请求参数类型不匹配，参数[%s]要求类型为：'%s'，但输入值为：'%s'", e.getName(), e.getRequiredType().getName(), e.getValue()));
     }
 
     /**
-     * 自定义验证异常
+     * 请求参数校验不通过
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.error(e.getMessage(), e);
+    public ResponseEntity<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e,
+                                                                      HttpServletRequest request) {
+        log.error("请求参数校验不通过: {},{} ", request.getRequestURI(), e.getMessage());
         String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
         return ResponseEntity.fail(message);
     }
@@ -116,8 +120,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Void> handleRuntimeException(RuntimeException e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',发生未知异常.", requestURI, e);
+        log.error("拦截位置运行时异常'{}',发生未知异常.", request.getRequestURI(), e);
         return ResponseEntity.fail(e.getMessage());
     }
 
@@ -125,8 +128,9 @@ public class GlobalExceptionHandler {
      * 自定义验证异常
      */
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Void> constraintViolationException(ConstraintViolationException e) {
-        log.error(e.getMessage(), e);
+    public ResponseEntity<Void> constraintViolationException(ConstraintViolationException e,
+                                                             HttpServletRequest request) {
+        log.error("自定义验证异常: {}, {} ", request.getRequestURI(), e.getMessage());
         String message = StreamUtils.join(e.getConstraintViolations(), ConstraintViolation::getMessage, ", ");
         return ResponseEntity.fail(message);
     }
@@ -159,7 +163,9 @@ public class GlobalExceptionHandler {
      * 拦截前端传参未正常接收错误
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleNestedServletException(HttpMessageNotReadableException ex) {
+    public ResponseEntity<String> handleNestedServletException(HttpMessageNotReadableException e,
+                                                               HttpServletRequest request) {
+        log.error("拦截前端传参未正常接收错误: {}，{} ", request.getRequestURI(), e.getMessage());
         return ResponseEntity.fail("未读取到请求体,请确认参数数据格式是否正确");
     }
 
@@ -169,8 +175,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Void> handleException(Exception e, HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',发生系统异常.", requestURI, e);
+        log.error("请求地址'{}',发生系统异常.", request.getRequestURI(), e);
         return ResponseEntity.fail(HttpStatus.HTTP_INTERNAL_ERROR, e.getMessage());
     }
 
